de2e33bbe1f3b9697618381629a2f764e935adb8
Jaron <jaronchretien@yahoo.com>
2023-04-29 11:58:58 -0500

    feat(submit): Adds submit incompletely

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
-
+import { useState } from "react"
 
 const App = () => {
+    let [day, setDay] = useState(null)
+    let [month, setMonth] = useState(null)
+    let [year, setYear] = useState(null)
+    let [calc, setCalc] = useState(null)
+
+    const submit = () => {
+        day.length < 2 ? day = `0${day}`: null
+
+        month.length < 2 ? month = `0${month}`: null
+
+        year.length == 1 ? year = `000${year}` : year.length == 2 ?  year = `00${year}`: year.length == 3 ? year = `0${year}`: null
+        
+        const chosenDate = new Date(`${year}-${month}-${day}T00:00:00`)
+        
+        console.log(chosenDate)
+        let mili = chosenDate.getTime()
+        
+        const years =  mili / 3.154e+10 
+
+        console.log(years)
+        
+
+
+    }
     return (
         <div className="mainContainer">
             <div className="informationContainer">
@@ -8,29 +32,29 @@ const App = () => {
                     
                     <div className="inputfield 1">
                     <label>DAY</label>
-                    <input></input>
+                    <input value={day} onChange={(e) => setDay(e.target.value)}></input>
                     </div>
 
                     <div className="inputfield 2">
                     <label>MONTH</label>
-                    <input></input>
+                    <input value={month} onChange={(e) => setMonth(e.target.value)}></input>
                     </div>
 
                     <div className="inputfield 3">
                     <label>YEAR</label>
-                    <input></input>
+                    <input value={year} onChange={(e) => setYear(e.target.value)}></input>
                     </div>
                 
                 </div>
                 <div className="buttonContainer">
                    <hr className="lineBreak"/>
-                   <img className="submitImage" src={'/assets/images/icon-arrow.svg'}/>
+                   <img onClick={submit} className="submitImage" src={'/assets/images/icon-arrow.svg'}/>
                 </div>
             </div>
             <div className="resultContainer">
-                <span><span className="resultData">--</span> years</span>
-                <span><span className="resultData">--</span> months</span>
-                <span><span className="resultData">--</span> days</span>
+                <span><span className="resultData">{calc || '--'}</span> years</span>
+                <span><span className="resultData">{calc || '--'}</span> months</span>
+                <span><span className="resultData">{calc || '--'}</span> days</span>
             </div>
       
         </div>
